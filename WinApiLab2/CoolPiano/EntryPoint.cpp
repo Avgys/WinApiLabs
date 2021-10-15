c2f16e92ca6cd71a79c4e26194ce9cdb9577375d
Ilya Kosykh <ilyuha.prosto.22041961@gmail.com>
2021-10-15 23:38:11 +0300

    done

--- WinApiLab2/CoolPiano/EntryPoint.cpp
+++ WinApiLab2/CoolPiano/EntryPoint.cpp
@@ -7,9 +7,11 @@
 #include <string>
 #include <time.h>
 #include <fstream>
+#include <math.h>
 
 using namespace std;
 
+#define PI 3.14159265 
 #define DELTA 3
 
 RECT windowRect;
@@ -17,9 +19,11 @@ HINSTANCE hInst;
 int rowCount = 10;
 int columnCount = 5;
 string** stringMatrix;
+int tableHeight;
 
 void CreateStringMatrix();
 void DrawTable(HDC hDC, RECT rect, int borderSize);
+void DrawCycle(HDC hDC, RECT rect);
 
 LRESULT WINAPI WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -46,6 +50,7 @@ LRESULT WINAPI WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_PAINT:
 		hDC = BeginPaint(hWnd, &ps);
 		DrawTable(hDC, windowRect, 3);
+		DrawCycle(hDC, windowRect);
 		EndPaint(hWnd, &ps);
 		break;
 	}
@@ -147,9 +152,27 @@ string getLongestString(int currentLine)
 	return longestSrting;
 }
 
-int GetBlockHeight(HDC hdc, int currentLine, int width)
+//int GetBlockHeight(HDC hdc, int currentLine, int width)
+//{
+//	string longestString = getLongestString(currentLine);
+//
+//	RECT nonDrawableBlock;
+//	nonDrawableBlock.left = 0;
+//	nonDrawableBlock.top = 0;
+//	nonDrawableBlock.bottom = 1;
+//	nonDrawableBlock.right = width;
+//
+//	string a = longestString.c_str();
+//	int b = longestString.length();
+//	std::wstring widestr = std::wstring(longestString.begin(), longestString.end());
+//	int height = (int)DrawText(hdc, widestr.c_str(), longestString.length(), &nonDrawableBlock,
+//		DT_CALCRECT | DT_WORDBREAK | DT_EDITCONTROL | DT_CENTER) + DELTA;
+//
+//	return GetWordHeight();
+//}
+
+int GetWordHeight(HDC hdc, string longestString, int width)
 {
-	string longestString = getLongestString(currentLine);
 
 	RECT nonDrawableBlock;
 	nonDrawableBlock.left = 0;
@@ -173,28 +196,27 @@ void DrawLine(HDC hdc, int x1, int y1, int x2, int y2)
 }
 
 
-void DrawTextBlock(HDC hdc, int left, int top, int width, int height, int row, int column, int borderSize)
+void DrawTextBlock(HDC hdc, int left, int top, int width, int height, std::wstring str, int borderSize, int angle = 0)
 {
 	RECT rect;
 	rect.top = (long)(top + borderSize);
-	//rect.top = (long)(100);
+	//rect.top = (long)(top);
 	rect.left = (long)(left + borderSize);
-	//rect.left = (long)(100);
+	//rect.left = (long)(left);
 	rect.right = (long)(left + width - borderSize);
-	//rect.right = (long)(left + width);
+	////rect.right = (long)(left + width);
 	rect.bottom = (long)(top + height - borderSize);
 	//rect.bottom = (long)(top + height);
 
-	std::wstring widestr = std::wstring(stringMatrix[row][column].begin(), stringMatrix[row][column].end());
 
-	DrawText(hdc, widestr.c_str(), stringMatrix[row][column].length(), &rect, DT_WORDBREAK | DT_EDITCONTROL | DT_CENTER);
+	DrawText(hdc, str.c_str(), str.length(), &rect, DT_WORDBREAK | DT_EDITCONTROL | DT_CENTER | DT_NOCLIP);
 }
 
-HFONT generateFont()
+HFONT generateFont(int angle = 0)
 {
 	int fnWeight = 400;
 
-	HFONT hFont = CreateFont(20, 7, 0, 0, fnWeight, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_OUTLINE_PRECIS,
+	HFONT hFont = CreateFont(20, 7, angle, 0, fnWeight, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_OUTLINE_PRECIS,
 		CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, VARIABLE_PITCH, TEXT("Times New Roman"));
 
 	return hFont;
@@ -211,17 +233,20 @@ void DrawTable(HDC hDC, RECT rect, int borderSize)
 
 
 	//DrawTextBlock(hDC, 80, 80, rect.right, rect.bottom, 1, 1, borderSize);
-	for (int i = 0; i < rowCount; i++)
+	for (int row = 0; row < rowCount; row++)
 	{
-		cellHeight = GetBlockHeight(hDC, i, cellWidth);
+		string longestString = getLongestString(row);
+		cellHeight = GetWordHeight(hDC, longestString, cellWidth);
+
 		posX = 0;
-		for (int j = 0; j < columnCount; j++)
+		for (int column = 0; column < columnCount; column++)
 		{
 			//hFont = generateFont();
 			//SelectObject(hDC, hFont);
 
+			std::wstring widestr = std::wstring(stringMatrix[row][column].begin(), stringMatrix[row][column].end());
 			DrawLine(hDC, (int)posX, (int)posY, (int)posX, (int)(posY + cellHeight));
-			DrawTextBlock(hDC, posX, posY, cellWidth, cellHeight, i, j, borderSize);
+			DrawTextBlock(hDC, posX, posY, cellWidth, cellHeight, widestr, borderSize);
 			posX += cellWidth;
 
 			//DeleteObject(hFont);
@@ -231,7 +256,45 @@ void DrawTable(HDC hDC, RECT rect, int borderSize)
 		DrawLine(hDC, 0, (int)posY, rect.right, (int)posY);
 		posY += cellHeight;
 	}
-
+	tableHeight = posY;
 	DeleteObject(hFont);
 	DrawLine(hDC, 0, (int)posY, rect.right, (int)posY);
+}
+
+void DrawCycle(HDC hDC, RECT rect)
+{
+	float posX = rect.right / 2, posY = tableHeight + (rect.bottom - tableHeight) / 2;
+
+	string word = stringMatrix[0][0];
+	//string word = "word";
+
+	double anglePerChar = 360 / word.length();
+
+	std::wstring widestr = std::wstring(word.begin(), word.end());
+	//DrawTextBlock(hDC, 80, 80, rect.right, rect.bottom, 1, 1, borderSize);
+
+	int cellHeight = GetWordHeight(hDC, word, 100);
+	int x = 0, y = 0, R = 100;
+
+	for (int i = 0; i < word.length(); i++)
+	{		
+		x = posX + R * pow(sin(i * anglePerChar * PI / 180), 1);
+		y = posY - R * pow(cos(i * anglePerChar * PI / 180), 1);
+		
+			//hFont = generateFont();
+			//SelectObject(hDC, hFont);
+			HFONT hFont = generateFont( - i * anglePerChar * 10);
+			SelectObject(hDC, hFont);
+
+			DrawTextBlock(hDC, x, y, 20, cellHeight, widestr.substr(i, 1), 0);
+
+
+			DeleteObject(hFont);
+
+			//DeleteObject(hFont);
+		
+	}
+
+	
+	//DrawLine(hDC, 0, (int)posY, rect.right, (int)posY);
 }
